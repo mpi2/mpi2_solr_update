9d836f15161e508b87f3edb480d665e230173e78
Asfand Qazi <aq2@sanger.ac.uk>
2012-09-24 16:58:20 +0100

    Make SolrUpdate::Config get loaded in Rails 3 apps as well

--- lib/solr_update/config.rb
+++ lib/solr_update/config.rb
@@ -1,12 +1,10 @@
 class SolrUpdate::Config
   def self.init_config
+    return if class_variable_defined?(:@@config)
     pre_parsed_config = YAML.load_file("#{Rails.root}/config/solr_update.yml")
     @@config = pre_parsed_config.fetch(Rails.env)
     @@config.merge!(pre_parsed_config['all'])
   end
-  private_class_method :init_config
-
-  init_config
 
   def self.[](key)
     return @@config[key]

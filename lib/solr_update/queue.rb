c9973f190e8c2c7318d2aa7e28a327be6eaeed18
Asfand Qazi <aq2@sanger.ac.uk>
2012-09-27 13:36:15 +0100

    Update variable names in SolrUpdate::Queue

--- lib/solr_update/queue.rb
+++ lib/solr_update/queue.rb
@@ -1,19 +1,19 @@
 class SolrUpdate::Queue
-  def self.enqueue_for_update(object)
-    SolrUpdate::Queue::Item.add(object, 'update')
+  def self.enqueue_for_update(object_or_reference)
+    SolrUpdate::Queue::Item.add(object_or_reference, 'update')
   end
 
-  def self.enqueue_for_delete(object)
-    SolrUpdate::Queue::Item.add(object, 'delete')
+  def self.enqueue_for_delete(object_or_reference)
+    SolrUpdate::Queue::Item.add(object_or_reference, 'delete')
   end
 
   def self.run
     proxy = SolrUpdate::IndexProxy::Allele.new
-    SolrUpdate::Queue::Item.process_in_order do |object_id, command_type|
+    SolrUpdate::Queue::Item.process_in_order do |reference, command_type|
       if command_type == 'update'
-        command = SolrUpdate::CommandFactory.create_solr_command_to_update_in_index(object_id)
+        command = SolrUpdate::CommandFactory.create_solr_command_to_update_in_index(reference)
       elsif command_type == 'delete'
-        command = SolrUpdate::CommandFactory.create_solr_command_to_delete_from_index(object_id)
+        command = SolrUpdate::CommandFactory.create_solr_command_to_delete_from_index(reference)
       end
       proxy.update(command)
     end

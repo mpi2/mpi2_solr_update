ef6a48a05411b5e938060b569880b4358bd9f756
Asfand Qazi <aq2@sanger.ac.uk>
2012-09-28 16:34:18 +0100

    Make targ rep URL generating helpers support cre excised URLs

--- test/unit/solr_update/util_test.rb
+++ test/unit/solr_update/util_test.rb
@@ -13,9 +13,19 @@ class SolrUpdate::UtilTest < ActiveSupport::TestCase
               @testobj.allele_image_url(55)
     end
 
+    should 'generate cre excised allele_image_url' do
+      assert_equal 'http://www.knockoutmouse.org/targ_rep/alleles/55/allele-image-cre',
+              @testobj.allele_image_url(55, :cre => true)
+    end
+
     should 'generate genbank_file_url' do
       assert_equal 'http://www.knockoutmouse.org/targ_rep/alleles/55/escell-clone-genbank-file',
               @testobj.genbank_file_url(55)
+    end
+
+    should 'generate cre excised genbank_file_url' do
+      assert_equal 'http://www.knockoutmouse.org/targ_rep/alleles/55/escell-clone-cre-genbank-file',
+              @testobj.genbank_file_url(55, :cre => true)
     end
 
   end
